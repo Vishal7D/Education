79c259a69e53d66d1fef00c4d287a8687e4257ea
Vishal7D <satdive011@gmail.com>
2023-10-19 17:49:56 +0530

    My First Commit

---
+++ src/test/java/com/test/LoginTest.java
@@ -0,0 +1,69 @@
+package com.test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.BaseClass.BaseClass;
+import com.pom.LoginPom;
+import com.utility.Utility;
+
+public class LoginTest extends BaseClass {
+
+	@BeforeMethod
+ public void setUp() {
+		
+	 try {
+		launchTheWeb();
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+ }
+	//@AfterMethod
+ public void tearDown() {
+	 driver.quit();
+ }
+    @Test
+    
+ public void loginTest() {
+    	LoginPom login = new LoginPom();
+	 login.setInputUsername(login.getUsername());
+	 login.setInputPassword(login.getPassword());
+	 login.loginButtonClick();
+	 
+ }
+    @Test
+    
+    public void testUsers() throws IOException {
+    	LoginPom loginPom = new LoginPom();
+    	Utility utility = new Utility();
+    	
+    	Sheet sh= utility.getSheet("Sheet1");
+    	
+        String key=	(String) utility.getSingleData(1, 0, sh);
+        String value=(String) utility.getSingleData(1, 1, sh);
+        
+      loginPom.setInputUsername(key);
+      loginPom.setInputPassword(value);
+      loginPom.loginButtonClick();
+      getDataFromExcel();
+      
+    }
+    public void getDataFromExcel() throws IOException {
+    	Utility utility = new Utility();
+    	
+    	Sheet sh= utility.getSheet("Sheet1");
+    	
+    	utility.getAllExcelData(sh);
+    //String key=	(String) utility.getSingleData(1, 0, sh);
+    //String value=(String) utility.getSingleData(1, 1, sh);
+    
+    		
+    }
+}
